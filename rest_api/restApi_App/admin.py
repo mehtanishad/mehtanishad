84835fb6f572953a1667b4ebcef9ea623d6f00a7
mehtanishad <mehtanishad317@gmail.com>
2023-08-08 12:16:21 +0530

    practice

--- rest_api/restApi_App/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-all_models = [Task]
-
-for md in all_models:
-    admin.site.register(md)
--- rest_api/restApi_App/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RestapiAppConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "restApi_App"
